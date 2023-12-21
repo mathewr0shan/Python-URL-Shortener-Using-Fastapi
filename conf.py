8e1b7fb1b24c764e7489377c677c5124151b43e3
mathewr0shan <154357801+mathewr0shan@users.noreply.github.com>
2023-12-21 23:03:28 +0530

    add LRU cache  conf.py

--- conf.py
+++ conf.py
@@ -1,5 +1,6 @@
 # shortener_app/config.py
 
+from functools import lru_cache
 from pydantic import BaseSettings
 
 class Settings(BaseSettings):
@@ -7,6 +8,7 @@ class Settings(BaseSettings):
     base_url: str = "http://localhost:8000"
     db_url: str = "sqlite:///./shortener.db"
 
+@lru_cache
 def get_settings() -> Settings:
     settings = Settings()
     print(f"Loading settings for: {settings.env_name}")
